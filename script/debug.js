c9fed03f53f387b8658c8a9b2402ab2b877d93e8
coder-newcomer <amzhakkeren225@gmail.com>
2022-12-14 14:00:09 +0700

    Update

--- script/debug.js
+++ script/debug.js
@@ -1,3 +1,4 @@
 // debug script for debugging and development purpose only
 console.warn('⚠: Debug mode on');
 document.getElementById('console').style.width = '85vw';
+document.querySelector('html').style.setProperty('--demo', 'block');
