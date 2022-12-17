68b2847433c3b6749cb7cfc112b6f10d7f508be4
coder-newcomer <amzhakkeren225@gmail.com>
2022-12-17 09:48:24 +0700

    🆙 DATE

--- script/script.js
+++ script/script.js
@@ -1,40 +1,19 @@
 //document.getElementById('exit').onclick = function () {close()};
-var zygote = {
+var z = {
   getParam: function () {
     if (location.href.indexOf('?') != -1) {
       return location.href.substr(location.href.indexOf('?') + 1, location.href.length).split('&');
-    };
-  }
-};var z = zygote;
+    }}
+};
 
-if (zygote.getParam() != undefined) {
-  if (zygote.getParam().includes('debug')) {
+if (z.getParam() != undefined) {
+  if (z.getParam().includes('debug')) {
     document.querySelector('body').append(document.createElement('script'));
     document.querySelector('body').querySelectorAll('script')[document.querySelector('body').querySelectorAll('script').length - 1].src = 'script/debug.js';
   };
-  if (zygote.getParam().includes('useFont')) {
+  if (z.getParam().includes('useFont')) {
     
   };
 };
 
-/*
-function sendmsg(text, withicon) {
-  var msg = document.getElementById('msg');
-  msg.prepend(document.createElement('span'));
-  var span = msg.querySelectorAll('span')[0];
-  span.className = 'send';
-  var icon = '<span class="send icon">  </span>   ';
-  if (withicon) {icon = icon.replaceAll('  ','->');};
-  span.innerHTML = icon + text;;
-};
-
-function getmsg(text, withicon) {
-  var msg = document.getElementById('msg');
-  msg.prepend(document.createElement('span'));
-  var span = msg.querySelectorAll('span')[0];
-  span.className = 'get';
-  var icon = '<span class="get icon">  </span>   ';
-  if (withicon) {icon = icon.replaceAll('  ','<-');};
-  span.innerHTML = icon + text;;
-};
-*/
+// some function on this script now lony work with jQuery
