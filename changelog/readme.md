1437b38b6fbb7ca905ccf27b67eb3bbea53ba4bc
coder-newcomer <amzhakkeren225@gmail.com>
2022-12-20 21:41:28 +0700

    yes

--- changelog/readme.md
+++ changelog/readme.md
@@ -1,11 +1,17 @@
 # 🕒 Changelog
 #### 2022-12-14
+---
 - Structure have been predefined, need some fix and clean it (i'm busy).
 - Console window have been created successfully.
  - Function for send and get message have been created but need some fix for. more type of message and styler issue.
  
 #### 2022-12-19
+---
 - Now using jQuery.
 - Console window and the function have been created.
  - Currently supported message type: [send, get, info, warn, error] and plain in another function.
 - Now developer decided to focusing on the main script of the game (consoleRPG.js) for future development and neglected the web gui for a while.
+
+#### 2022-12-20
+---
+- Basic Register, Login, and Logout method have been implemented, but still incomplete in some aspect, and maybe need to cleanup the code.
--- script/consoleRPG-ported.js
+++ script/consoleRPG-ported.js
@@ -1,6 +1,7 @@
 var game = {
-  register: function(username, password, nickname) {
-    var json = {
+  profile: {
+    register: function(username, password, nickname) {
+      var json = {
   "user": {
     "name": "admin",
     "password": "admin",
@@ -9,20 +10,40 @@ var game = {
     "money": "9999999"
   }
 };
-    json.user.name = nickname;
-    json.user.password = password;
-    if (localStorage.getItem('user') == null) {localStorage.setItem('user','');};
-    var ussr = localStorage.getItem('user').split(',');
-    localStorage.setItem('user', ussr);
-    localStorage.setItem('user: ' + username, JSON.stringify(json));
-  },
-  login: function (username, password) {
-    if (localStorage.getItem(username) == null) {return console.error('Username not available!');};
-    var json = JSON.parse(localStorage.getItem('user: ' + username));
-    if (password == null) {return console.error('Input password!')};
-    if (password == json.user.password) {
-      localStorage.setItem('currentuser', username);
-      console.log ('Login success');
-    };
+      json.user.name = nickname;
+      json.user.password = password;
+      if (localStorage.getItem('user') == null) {localStorage.setItem('user', '');};
+      var ussr = localStorage.getItem('user').split(',');
+      ussr.push(username);
+      localStorage.setItem('user', ussr);
+      if (localStorage.getItem('user').indexOf(',') == 0) {localStorage.setItem('user', localStorage.getItem('user').replace(',', ''));};
+      localStorage.setItem('user: ' + username, JSON.stringify(json));
+      console.log('Registered');
+    },
+    login: function (username, password) {
+      if (localStorage.getItem('user: ' + username) == null) {return console.error('Username not available!');};
+      var json = JSON.parse(localStorage.getItem('user: ' + username));
+      if (password == null) {return console.error('Input password!')};
+      if (password == json.user.password) {
+        localStorage.setItem('currentuser', username);
+        if (localStorage.getItem('currentuser') == username) {
+          console.log ('Login success');
+        } else {console.warn('Something wrong!');};
+      };
+    },
+    logout: function () {
+      localStorage.setItem('currentuser', '')
+      if (localStorage.getItem('currentuser') == '') {
+        console.log('Logout success');
+      } else {console.warn('Something wrong!');}
+    },
+    reset: function (username, password) {
+      if (username == null && password == null) {
+        var msg = 'You are attempted to delete all user profile, you will be logout and your profile will be also deleted too. Are you sure?';
+        if (confirm(msg) == true) {
+          //localStorage.removeItem()
+        };
+      };
+    }
   }
 };
