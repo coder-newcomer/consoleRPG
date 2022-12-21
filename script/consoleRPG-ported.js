4a788b8e72ef2823f15c28f534b10323e5429711
coder-newcomer <amzhakkeren225@gmail.com>
2022-12-21 19:12:22 +0700

    Update consoleRPG-ported.js

--- script/consoleRPG-ported.js
+++ script/consoleRPG-ported.js
@@ -1,27 +1,29 @@
 var game = {
   profile: {
     register: function(username, password, nickname) {
-      var json = {
-  "user": {
-    "name": "admin",
-    "password": "admin",
-    "level": "99",
-    "xp": "9999999",
-    "money": "9999999"
-  }
-};
+      // Setting JSON template as savegame
+      var json = { "user": {"name": "","password": ""} };
       json.user.name = nickname;
       json.user.password = password;
-      if (localStorage.getItem('user') == null) {localStorage.setItem('user', '');};
-      var ussr = localStorage.getItem('user').split(',');
-      ussr.push(username);
-      localStorage.setItem('user', ussr);
-      if (localStorage.getItem('user').indexOf(',') == 0) {localStorage.setItem('user', localStorage.getItem('user').replace(',', ''));};
+      // Checking if userlist (user) on localStorage is exist, otherwise create new one with empty value to prevent error
+      if (localStorage.user == null) {localStorage.user = '';};
+      // Adding user value as list (using Array)
+      var ussr = localStorage.user.split(',');
+      ussr.push(username); 
+      localStorage.user = ussr;
+      // Store value as stringified Array
+      if (localStorage.user.indexOf(',') == 0) {localStorage.user = localStorage.user.replace(',', '');};
+      // Store user data (savegame) as stringified JSON
       localStorage.setItem('user: ' + username, JSON.stringify(json));
-      console.log('Registered');
+      // Return status when success
+      if (localStorage.getItem('user: ' + username) == JSON.stringify(json)) {
+        console.log('Registered');
+      } else {console.warn('Something wrong!')};
     },
     login: function (username, password) {
+      // check inputed username existence, otherwise go ahead
       if (localStorage.getItem('user: ' + username) == null) {return console.error('Username not available!');};
+      // Getting value and start verifying
       var json = JSON.parse(localStorage.getItem('user: ' + username));
       if (password == null) {return console.error('Input password!')};
       if (password == json.user.password) {
@@ -29,7 +31,7 @@ var game = {
         if (localStorage.getItem('currentuser') == username) {
           console.log ('Login success');
         } else {console.warn('Something wrong!');};
-      };
+      } else {console.error('Username or password is incorrect')};
     },
     logout: function () {
       localStorage.setItem('currentuser', '')
@@ -37,13 +39,19 @@ var game = {
         console.log('Logout success');
       } else {console.warn('Something wrong!');}
     },
-    reset: function (username, password) {
+    clear: function (username, password) {
       if (username == null && password == null) {
         var msg = 'You are attempted to delete all user profile, you will be logout and your profile will be also deleted too. Are you sure?';
         if (confirm(msg) == true) {
           //localStorage.removeItem()
         };
       };
-    }
+    },
+    delete: function (username, password) {return experimental();},
+    export: function (username, password) {return experimental();},
+    import: function (username, password) {return experimental();},
+    reset: function (username, password) {return experimental();}
   }
 };
+
+function experimental() {return console.log('Coming soon!')};
