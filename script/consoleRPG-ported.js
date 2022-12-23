cdacb6c456a1ea239aa34ed15ea2050103305028
coder-newcomer <amzhakkeren225@gmail.com>
2022-12-23 19:51:55 +0700

    Added language support

--- script/consoleRPG-ported.js
+++ script/consoleRPG-ported.js
@@ -1,45 +1,25 @@
-var currentlang = navigator.language; // For now language is managed by browser ui, but next will use an option to choose
+var currentlang = navigator.language; // Currently language is managed by browser ui, but next will use an option to choose
 var game = {
   xml: '<test>yes</test>', // XML testing, alternative translation for JSON if possible
   lang: {
-    example: {
-      "stringname": {
-        "langid": "translate",
-        "langid2": "translate"
-      },
-      test: {
-        "en": "The quick brown fox, jumps over the lazy dog. Select the answer: [a, b, c, d]",
-        "id": "Seekor rubah coklat yang cepat, melompat diatas anjing yang malas. Pilih jawaban: [a, b, c, d]",
-        "un": "Hey, you! this is: whatever." // Undefined language, just for testing
-      }
-    },
     parse: function (stringid, langid) { // language parse function for multi-language support
       var lang = JSON.stringify(stringid).replaceAll('{"','').replaceAll('"}', '').replaceAll('","', '":"').split('":"');
       // Error handler
       var err = 'The specified stringid object doesn\'t have a valid translation on it\'s own constructor following the specified langid value. Make sure you specify the right langid value or the specified stringid doesn\'t have the right translation that match with the specified langid value.';
-      var condition = [!JSON.stringify(stringid).includes('"langid":'.replaceAll('langid', langid)), lang.indexOf(langid) < 0]; // Two types of error condition checking [0, 1]
-      if (typeof stringid != 'object') {throw new TypeError('Specified stringid must be an valid language object! "%error" is\'nt an object!'.replaceAll('%error', stringid));} // Specified stringid must be an object
-      else if (!JSON.stringify(stringid).includes('"en":')) {throw new TypeError('Specified stringid is invalid language object!')} // Validate the stringid object constructor if they have at least one default translation langid (en = English)
+      var condition = [!JSON.stringify(stringid).includes('"langid":'.replaceAll('langid', langid)), lang.indexOf(langid) < 0]; // Two types of error condition checking [0,1]
+      if (typeof stringid != 'object') {throw new ReferenceError('Specified stringid must be an valid language object! "%error" is\'nt an object!'.replaceAll('%error', stringid));} // Specified stringid must be an object
+      else if (!JSON.stringify(stringid).includes('"en":')) {throw new TypeError('Specified stringid is invalid language object!')} // Validate the stringid object constructor if they have at least one default translation langid (Default = en)
       else if (langid.length > 2 || typeof langid != 'string') {throw new TypeError('Unknown specified langid value!');} // Currently the valid langid value is 2 digit string following in the format defined in RFC 5646: Tags for Identifying Languages (also known as BCP 47)
       else if (condition[1]) {throw new Error(err);} // This error handler is unexpected sometimes, use try catch i think
       else {return lang[lang.indexOf(langid) + 1];}
-    },
-    // Translation goes here
-    // Currently supported language: [en = English, id = Indonesia]
-    "registered": {
-      "en": "Registered",
-      "id": "Terdaftar"
-    },
-    "wrong1": {
-      "en": "Something wrong!",
-      "id": "Ada sesuatu yang salah!"
     }
-    // End here
   },
   profile: {
     register: function(username, password, nickname) {
       // Setting JSON template as savegame
       var json = { "user": {"name": "","password": ""} };
+      if (nickname == undefined) {nickname = ''};
+      if (password == undefined) {password = ''};
       json.user.name = nickname;
       json.user.password = password;
       // Checking if userlist (user) on localStorage is exist, otherwise create new one with empty value to prevent error
@@ -54,27 +34,27 @@ var game = {
       localStorage.setItem('user: ' + username, JSON.stringify(json));
       // Return status when success
       if (localStorage.getItem('user: ' + username) == JSON.stringify(json)) {
-        console.log(game.lang.registered.en); // <-- fix from this point 
-      } else {console.warn('Something wrong!')};
+        console.log(game.lang.parse(game.lang.profile.registered, currentlang)); // <-- fix from this point 
+      } else {console.warn(game.lang.parse(game.lang.profile.wrong1, currentlang))};
     },
     login: function (username, password) {
-      // check specified username existence, otherwise go ahead
-      if (localStorage.getItem('user: ' + username) == null) {return console.error('Username not available!');};
+      // Check specified username existence, otherwise go ahead
+      if (localStorage.getItem('user: ' + username) == null) {return console.error(game.lang.parse(game.lang.profile.usernotexist, currentlang));};
       // Getting value and start verifying
       var json = JSON.parse(localStorage.getItem('user: ' + username));
-      //if (password == null) {return console.error('Input password!')}; // Need to add exception when username has no password
+      if (password == undefined) {password = ''}; // Need to add exception when username has no password
       if (password == json.user.password) {
         localStorage.currentuser = username;
         if (localStorage.currentuser == username) {
-          console.log ('Login success');
-        } else {console.warn('Something wrong!');};
-      } else {console.error('Username or password is incorrect')};
+          console.log (game.lang.parse(game.lang.profile.loginsuccess, currentlang));
+        } else {console.warn(game.lang.parse(game.lang.profile.wrong1, currentlang));};
+      } else {console.error(game.lang.parse(game.lang.profile.userpwincorrect, currentlang))};
     },
     logout: function () {
-      localStorage.setItem('currentuser', '')
-      if (localStorage.getItem('currentuser') == '') {
-        console.log('Logout success');
-      } else {console.warn('Something wrong!');}
+      localStorage.currentuser = '';
+      if (localStorage.currentuser == '') {
+        console.log(game.lang.parse(game.lang.profile.logoutsuccess, currentlang));
+      } else {console.warn(game.lang.parse(game.lang.profile.wrong1, currentlang));}
     },
     clear: function () {
       var msg = 'You are attempted to delete all user profile, you will be logout and your profile will be also deleted too. Are you sure?';
@@ -87,4 +67,4 @@ var game = {
   }
 };
 
-function experimental() {return console.log('Comming soon!')};
+function experimental() {return console.log(game.lang.parse(game.lang.example.experimental, currentlang))};
---
+++ script/consoleRPG-ported.lang.js
@@ -0,0 +1,43 @@
+game.lang.example = { // Example usage of Translation | Do not edit!
+  "stringname": {
+    "langid": "translate",
+    "langid2": "translate"
+  },
+  test: {
+    "en": "The quick brown fox, jumps over the lazy dog. Select the answer: [a, b, c, d]",
+    "id": "Seekor rubah coklat yang cepat, melompat diatas anjing yang malas. Pilih jawaban: [a, b, c, d]",
+    "un": "Hey, you! this is: whatever." // Undefined language, just for testing
+  },
+  "experimental": {
+    "en": "Coming soon!",
+    "id": "Segera!"
+  }
+}
+game.lang.profile = {
+  // Translation goes here
+  // Currently supported language: [en = English, id = Indonesia]
+  "registered": {
+    "en": "Registered!",
+    "id": "Terdaftar!"
+  },
+  "wrong1": {
+    "en": "Something wrong!",
+    "id": "Ada sesuatu yang salah!"
+  },
+  "usernotexist": {
+    "en": "Username isn't exist!",
+    "id": "Username tidak ada!"
+  },
+  "userpwincorrect": {
+    "en": "Username or password is incorrect!",
+    "id": "Username atau password salah!"
+  },
+  "loginsuccess": {
+    "en": "Login success!",
+    "id": "Login berhasil!"
+  },
+  "logoutsuccess": {
+    "en": "Logout success!",
+    "id": "Logout berhasil!"
+  }
+};
